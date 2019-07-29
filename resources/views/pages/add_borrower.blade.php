c1a4c41e3cd141bb151846873c3add7b65453ec6
HridoyVaraby <sbi.ridoy@gmail.com>
2019-07-29 21:21:02 +0600

    page updated

--- resources/views/pages/add_borrower.blade.php
+++ resources/views/pages/add_borrower.blade.php
@@ -1,47 +1,75 @@
 @extends('layouts.appnev')
 
 @section('content')
-	<div class="row Add_Borrower">
+<!-- Add Borrower Section -->
+<div class="row Add_borrower">
+	<div class="row">
 		<div class="col-md-12">
-			<form role="form" class="form-horizontal form-groups-bordered">
-				<div class="form-group">
-					<label for="field-1" class="col-sm-3 control-label">
-						Name
-					</label>
-					<div class="col-md-6">
-						<input type="text" class="form-control" id="field-A" placeholder="Placeholder">
+			<div class="panel panel-primary" data-collapsed="0">
+				<div class="panel-heading">
+					<div class="panel-title">
+						Add Borrower
 					</div>
-				</div>
-				<div class="form-group">
-					<label for="field-1" class="col-sm-3 control-label">
-						Address
-					</label>
-					<div class="col-md-6">
-						<input type="text" class="form-control" id="field-A" placeholder="Placeholder">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="field-1" class="col-sm-3 control-label">
-						Phone
-					</label>
-					<div class="col-md-6">
-						<input type="text" class="form-control" id="field-A" placeholder="Placeholder">
-					</div>
-				</div>
-				<div class="form-group">
-					<label for="field-1" class="col-sm-3 control-label">
-						Borrow Return
-					</label>
-					<div class="col-md-6">
-						<input type="text" class="form-control" id="field-A" placeholder="Placeholder">
+					<div class="panel-options">
+						<a href="#sample-modal" data-toggle="modal" data-target="#sample-modal-dialog-1" class="bg" ><i class="entypo-cog"></i ></a>
+						<a href="{{ url('/list-borrowers')}}" data-rel="list"><i class="entypo-list"></i></a>
+						<a href="#" data-rel="collapse"><i class="entypo-down-open"></i></a>
+						<a href="#" data-rel="reload"><i class="entypo-arrows-ccw"></i></a>
+						<a href="#" data-rel="close"><i class="entypo-cancel"></i></a>
 					</div>
 				</div>
 
-				<div class="Submit_Button_ col-md-6 text-right">
-					<button class="btn btn-success">Submit</button>
+						@if (session('status'))
+							<div class="alert alert-success">
+								{{ session('status') }}
+							</div>
+						@endif
+						@foreach ($errors->all() as $error)
+							<div class="alert alert-danger">
+								<li>{{ $error }}</li>
+							</div>
+						@endforeach
+	
+
+				<div class="panel-body">
+					<form role="form" class="form-horizontal form-groups" action="{{ url('/add-borrower-insert')}}" method="post">
+						@csrf
+						<div class="form-group">
+							<label for="field-1" class="col-sm-3 control-label">
+								Borrower Name
+							</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" id="field-A" name="borrower_name" placeholder="Enter borrower name" value="{{ old('borrower_name') }}">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="field-1" class="col-sm-3 control-label">
+								Borrower Address
+							</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control" id="field-A" name="borrower_address" placeholder="Enter borrower address" value="{{ old('borrower_address') }}">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="field-1" class="col-sm-3 control-label">
+								Borrower Phone No
+							</label>
+							<div class="col-md-4">
+								<input type="tel" pattern="[0-9]{11}" class="form-control" id="field-A" name="borrower_phone" placeholder="Enter borrower phone no" value="{{ old('borrower_phone') }}">
+							</div>
+							<div class="col-md-4">
+								<p class="text-muted">Example: 01722895296</p>
+							</div>
+						</div>
+
+						<div class="Submit_Button_ col-md-6 text-right">
+							<button class="btn btn-success">Submit</button>
+						</div>
+					</form>
 				</div>
-			</form>
+			</div>
 		</div>
-	</div>
-	<!-- Add Borrower section Ends -->
+</div>
+<!-- Add borrower section Ends --
+
 @endsection
